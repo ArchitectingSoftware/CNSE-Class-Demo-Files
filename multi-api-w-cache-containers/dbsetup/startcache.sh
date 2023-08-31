4168b761a0a467f085d88b4c4cd54252860be859
Brian Mitchell <dr.brian.mitchell@gmail.com>
2023-08-31 14:50:04 -0400

    some fixes

--- multi-api-w-cache-containers/dbsetup/startcache.sh
+++ multi-api-w-cache-containers/dbsetup/startcache.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --rm --name pubcache -v "./dbdata/:/data" -e REDIS_ARGS="--appendonly yes"  -p 6379:6379 -p 8001:8001 redis/redis-stack:latest
+docker run -d --rm --name pubcache -v "/tmp/cnse-data:/data" -e REDIS_ARGS="--appendonly yes"  -p 6379:6379 -p 8001:8001 redis/redis-stack:latest
