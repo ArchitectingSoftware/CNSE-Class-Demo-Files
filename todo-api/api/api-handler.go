10696709633b62bacaa0e12236a5dbb531ec19c8
Brian Mitchell <dr.brian.mitchell@gmail.com>
2024-01-25 16:18:59 -0500

    updating todo api

--- todo-api/api/api-handler.go
+++ todo-api/api/api-handler.go
@@ -1,12 +1,14 @@
 package api
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 
 	"drexel.edu/todo/db"
-	"github.com/gin-gonic/gin"
+	"github.com/gofiber/fiber/v2"
 )
 
 // The api package creates and maintains a reference to the data handler
@@ -37,13 +39,13 @@ func New() (*ToDoAPI, error) {
 
 // implementation for GET /todo
 // returns all todos
-func (td *ToDoAPI) ListAllTodos(c *gin.Context) {
+func (td *ToDoAPI) ListAllTodos(c *fiber.Ctx) error {
 
 	todoList, err := td.db.GetAllItems()
 	if err != nil {
 		log.Println("Error Getting All Items: ", err)
-		c.AbortWithStatus(http.StatusNotFound)
-		return
+		return fiber.NewError(http.StatusNotFound,
+			"Error Getting All Items")
 	}
 	//Note that the database returns a nil slice if there are no items
 	//in the database.  We need to convert this to an empty slice
@@ -53,7 +55,7 @@ func (td *ToDoAPI) ListAllTodos(c *gin.Context) {
 		todoList = make([]db.ToDoItem, 0)
 	}
 
-	c.JSON(http.StatusOK, todoList)
+	return c.JSON(todoList)
 }
 
 // implementation for GET /v2/todo
@@ -62,13 +64,12 @@ func (td *ToDoAPI) ListAllTodos(c *gin.Context) {
 // for example, /v2/todo?done=true will return all
 // todos that are done.  Note you can have multiple
 // query parameters, for example /v2/todo?done=true&foo=bar
-func (td *ToDoAPI) ListSelectTodos(c *gin.Context) {
+func (td *ToDoAPI) ListSelectTodos(c *fiber.Ctx) error {
 	//lets first load the data
 	todoList, err := td.db.GetAllItems()
 	if err != nil {
 		log.Println("Error Getting Database Items: ", err)
-		c.AbortWithStatus(http.StatusNotFound)
-		return
+		return fiber.NewError(http.StatusNotFound)
 	}
 	//If the database is empty, make an empty slice so that the
 	//JSON marshalling works correctly
@@ -82,8 +83,7 @@ func (td *ToDoAPI) ListSelectTodos(c *gin.Context) {
 
 	//if the doneS is empty, then we will return all items
 	if doneS == "" {
-		c.JSON(http.StatusOK, todoList)
-		return
+		return c.JSON(todoList)
 	}
 
 	//Now we can handle the case where doneS is not empty
@@ -92,8 +92,7 @@ func (td *ToDoAPI) ListSelectTodos(c *gin.Context) {
 	done, err := strconv.ParseBool(doneS)
 	if err != nil {
 		log.Println("Error converting done to bool: ", err)
-		c.AbortWithStatus(http.StatusBadRequest)
-		return
+		return fiber.NewError(http.StatusBadRequest)
 	}
 
 	//Now we need to filter the list based on the done value
@@ -114,41 +113,37 @@ func (td *ToDoAPI) ListSelectTodos(c *gin.Context) {
 		filteredList = make([]db.ToDoItem, 0)
 	}
 
-	c.JSON(http.StatusOK, filteredList)
+	return c.JSON(filteredList)
 }
 
 // implementation for GET /todo/:id
 // returns a single todo
-func (td *ToDoAPI) GetToDo(c *gin.Context) {
+func (td *ToDoAPI) GetToDo(c *fiber.Ctx) error {
 
 	//Note go is minimalistic, so we have to get the
 	//id parameter using the Param() function, and then
 	//convert it to an int64 using the strconv package
-	idS := c.Param("id")
-	id64, err := strconv.ParseInt(idS, 10, 32)
+	id, err := c.ParamsInt("id")
 	if err != nil {
-		log.Println("Error converting id to int64: ", err)
-		c.AbortWithStatus(http.StatusBadRequest)
-		return
+		return fiber.NewError(http.StatusBadRequest)
 	}
 
 	//Note that ParseInt always returns an int64, so we have to
 	//convert it to an int before we can use it.
-	todoItem, err := td.db.GetItem(int(id64))
+	todoItem, err := td.db.GetItem(id)
 	if err != nil {
 		log.Println("Item not found: ", err)
-		c.AbortWithStatus(http.StatusNotFound)
-		return
+		return fiber.NewError(http.StatusNotFound)
 	}
 
 	//Git will automatically convert the struct to JSON
 	//and set the content-type header to application/json
-	c.JSON(http.StatusOK, todoItem)
+	return c.JSON(todoItem)
 }
 
 // implementation for POST /todo
 // adds a new todo
-func (td *ToDoAPI) AddToDo(c *gin.Context) {
+func (td *ToDoAPI) AddToDo(c *fiber.Ctx) error {
 	var todoItem db.ToDoItem
 
 	//With HTTP based APIs, a POST request will usually
@@ -157,71 +152,67 @@ func (td *ToDoAPI) AddToDo(c *gin.Context) {
 	//we need to bind the JSON to a struct that we
 	//can use in our code.
 	//This framework exposes the raw body via c.Request.Body
-	//but it also provides a helper function ShouldBindJSON()
+	//but it also provides a helper function BodyParser
 	//that will extract the body, convert it to JSON and
 	//bind it to a struct for us.  It will also report an error
 	//if the body is not JSON or if the JSON does not match
 	//the struct we are binding to.
-	if err := c.ShouldBindJSON(&todoItem); err != nil {
+	if err := c.BodyParser(&todoItem); err != nil {
 		log.Println("Error binding JSON: ", err)
-		c.AbortWithStatus(http.StatusBadRequest)
-		return
+		return fiber.NewError(http.StatusBadRequest)
 	}
 
 	if err := td.db.AddItem(todoItem); err != nil {
 		log.Println("Error adding item: ", err)
-		c.AbortWithStatus(http.StatusInternalServerError)
-		return
+		return fiber.NewError(http.StatusInternalServerError)
 	}
 
-	c.JSON(http.StatusOK, todoItem)
+	return c.JSON(todoItem)
 }
 
 // implementation for PUT /todo
 // Web api standards use PUT for Updates
-func (td *ToDoAPI) UpdateToDo(c *gin.Context) {
+func (td *ToDoAPI) UpdateToDo(c *fiber.Ctx) error {
 	var todoItem db.ToDoItem
-	if err := c.ShouldBindJSON(&todoItem); err != nil {
+	if err := c.BodyParser(&todoItem); err != nil {
 		log.Println("Error binding JSON: ", err)
-		c.AbortWithStatus(http.StatusBadRequest)
-		return
+		return fiber.NewError(http.StatusBadRequest)
 	}
 
 	if err := td.db.UpdateItem(todoItem); err != nil {
 		log.Println("Error updating item: ", err)
-		c.AbortWithStatus(http.StatusInternalServerError)
-		return
+		return fiber.NewError(http.StatusInternalServerError)
 	}
 
-	c.JSON(http.StatusOK, todoItem)
+	return c.JSON(todoItem)
 }
 
 // implementation for DELETE /todo/:id
 // deletes a todo
-func (td *ToDoAPI) DeleteToDo(c *gin.Context) {
-	idS := c.Param("id")
-	id64, _ := strconv.ParseInt(idS, 10, 32)
-
-	if err := td.db.DeleteItem(int(id64)); err != nil {
-		log.Println("Error deleting item: ", err)
-		c.AbortWithStatus(http.StatusInternalServerError)
-		return
+func (td *ToDoAPI) DeleteToDo(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return fiber.NewError(http.StatusBadRequest)
 	}
 
-	c.Status(http.StatusOK)
+	if err := td.db.DeleteItem(id); err != nil {
+		log.Println("Error deleting item: ", err)
+		return fiber.NewError(http.StatusInternalServerError)
+	}
+
+	return c.Status(http.StatusOK).SendString("Delete OK")
 }
 
 // implementation for DELETE /todo
 // deletes all todos
-func (td *ToDoAPI) DeleteAllToDo(c *gin.Context) {
+func (td *ToDoAPI) DeleteAllToDo(c *fiber.Ctx) error {
 
 	if err := td.db.DeleteAll(); err != nil {
 		log.Println("Error deleting all items: ", err)
-		c.AbortWithStatus(http.StatusInternalServerError)
-		return
+		return fiber.NewError(http.StatusInternalServerError)
 	}
 
-	c.Status(http.StatusOK)
+	return c.Status(http.StatusOK).SendString("Delete All OK")
 }
 
 /*   SPECIAL HANDLERS FOR DEMONSTRATION - CRASH SIMULATION AND HEALTH CHECK */
@@ -229,18 +220,39 @@ func (td *ToDoAPI) DeleteAllToDo(c *gin.Context) {
 // implementation for GET /crash
 // This simulates a crash to show some of the benefits of the
 // gin framework
-func (td *ToDoAPI) CrashSim(c *gin.Context) {
+func (td *ToDoAPI) CrashSim(c *fiber.Ctx) error {
 	//panic() is go's version of throwing an exception
+	//note with recover middleware this will not end program
 	panic("Simulating an unexpected crash")
+}
+
+func (td *ToDoAPI) CrashSim2(c *fiber.Ctx) error {
+	//A stupid crash simulation example
+	i := 0
+	j := 1 / i
+	jStr := fmt.Sprintf("%d", j)
+	return c.Status(http.StatusOK).
+		JSON(fiber.Map{
+			"val_j": jStr,
+		})
+}
+
+func (td *ToDoAPI) CrashSim3(c *fiber.Ctx) error {
+	//A stupid crash simulation example
+	os.Exit(10)
+	return c.Status(http.StatusOK).
+		JSON(fiber.Map{
+			"error": "will never get here, nothing you can do about this",
+		})
 }
 
 // implementation of GET /health. It is a good practice to build in a
 // health check for your API.  Below the results are just hard coded
 // but in a real API you can provide detailed information about the
 // health of your API with a Health Check
-func (td *ToDoAPI) HealthCheck(c *gin.Context) {
-	c.JSON(http.StatusOK,
-		gin.H{
+func (td *ToDoAPI) HealthCheck(c *fiber.Ctx) error {
+	return c.Status(http.StatusOK).
+		JSON(fiber.Map{
 			"status":             "ok",
 			"version":            "1.0.0",
 			"uptime":             100,
